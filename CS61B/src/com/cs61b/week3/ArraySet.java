5eefef3bcc701ca77f4eecb6d060d676db5400ba
JoeAuthenticStudios <92136840+JoeAuthenticStudios@users.noreply.github.com>
2022-04-17 12:19:09 -0700

    arrayset

--- CS61B/src/com/cs61b/week3/ArraySet.java
+++ CS61B/src/com/cs61b/week3/ArraySet.java
@@ -1,5 +1,33 @@
 package com.cs61b.week3;
 
-public class ArraySet {
-
+public class ArraySet <T> {
+	private T[] items;
+	private int size; 
+	
+	public ArraySet() {
+		items = (T[]) new Object[100];
+		size = 0; 
+	}
+	
+	public boolean contains(T x) {
+		for(int i = 0; i < size; i++) {
+			if(x.equals(items[i])) {
+				return true; 
+			}
+		}
+		return false; 
+	}
+	
+	
+	public void add(T x) {
+		if(contains(x)) {
+			return; 
+		}
+		items[size] = x; 
+		size += 1; 
+	}
+	
+	public int size() {
+		return size; 
+	}
 }
